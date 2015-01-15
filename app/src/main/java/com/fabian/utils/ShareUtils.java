dbc5ec88a6493b4ba2b5efd5a141762d7fff708a
liwangjun <liwangjun@173-168-1-101.res.bhn.net>
2015-01-15 21:53:13 +0800

    Initial commit

---
+++ app/src/main/java/com/fabian/utils/ShareUtils.java
@@ -0,0 +1,19 @@
+package com.fabian.utils;
+
+import android.util.Log;
+
+/**
+ * Created by liwangjun on 14/12/12.
+ */
+public class ShareUtils {
+
+    /**
+     * this is parm
+     * @param content
+     * @param imagePath
+     */
+    public void shareToTencent(String content,String imagePath){
+        Log.i("Dream",content);
+
+    }
+}
